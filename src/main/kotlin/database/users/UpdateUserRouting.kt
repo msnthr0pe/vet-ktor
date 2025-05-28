1740719b6ee656485b9a94d99eef9aa790e85d03
msnthr0pe <pavlid66@gmail.com>
2025-05-28 13:01:16 +0300

    Done UpdateUserRouting.kt

--- src/main/kotlin/database/users/UpdateUserRouting.kt
+++ src/main/kotlin/database/users/UpdateUserRouting.kt
@@ -5,24 +5,32 @@ import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.jetbrains.exposed.sql.update
+import javax.management.Query.and
 
 fun Application.configureUpdateUserRouting() {
     routing {
         post("/updateuser") {
-            val updateRequest = call.receive<PasswordDTO>()
+            val updateRequest = call.receive<UserDTO>()
 
             val rowsUpdated = transaction {
-                Users.update({ Users.login eq updateRequest.email }) {
-                    it[password] = updateRequest.password
+                Users.update({
+                    (Users.login eq updateRequest.login) and
+                        (Users.password eq updateRequest.password)
+                }) {
+                    it[name] = updateRequest.name
+                    it[surname] = updateRequest.surname
+                    it[phone] = updateRequest.phone
                 }
             }
 
             if (rowsUpdated > 0) {
-                call.respond(EmailDTO("Пароль изменён"))
+                call.respond(InfoDTO("Данные изменены"))
             } else {
-                call.respond(EmailDTO("Пользователь не найден"))
+                call.respond(InfoDTO("Ошибка изменения данных"))
             }
         }
     }
